ebd61f640e8ea3b7016b75be8f0dea3491622d74
Enzo Volkmann <enzo@exportarts.io>
2020-01-03 10:31:33 +0100

    feat: added config for favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,6 +65,50 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#ff5975`,
         display: `standalone`,
+        // TODO: Specify your favicon source file and un-comment the following lines
+        // icon: './path/to/favicon.svg',
+        // icons: [
+        //   {
+        //     src: `favicon/icon-48x48.png`,
+        //     sizes: `48x48`,
+        //     type: `image/png`,
+        //   },
+        //   {
+        //     src: `favicon/icon-72x72.png`,
+        //     sizes: `72x72`,
+        //     type: `image/png`,
+        //   },
+        //   {
+        //     src: `favicon/icon-96x96.png`,
+        //     sizes: `96x96`,
+        //     type: `image/png`,
+        //   },
+        //   {
+        //     src: `favicon/icon-144x144.png`,
+        //     sizes: `144x144`,
+        //     type: `image/png`,
+        //   },
+        //   {
+        //     src: `favicon/icon-192x192.png`,
+        //     sizes: `192x192`,
+        //     type: `image/png`,
+        //   },
+        //   {
+        //     src: `favicon/icon-256x256.png`,
+        //     sizes: `256x256`,
+        //     type: `image/png`,
+        //   },
+        //   {
+        //     src: `favicon/icon-384x384.png`,
+        //     sizes: `384x384`,
+        //     type: `image/png`,
+        //   },
+        //   {
+        //     src: `favicon/icon-512x512.png`,
+        //     sizes: `512x512`,
+        //     type: `image/png`,
+        //   },
+        // ]
       },
     },
     {
